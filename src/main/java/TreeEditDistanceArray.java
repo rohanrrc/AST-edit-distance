9a78ead74c49742685761603b5e86ab39b66c641
Rohan Roy Choudhury <rrc@berkeley.edu>
2016-03-31 18:13:31 -0700

    bug fix

--- src/main/java/TreeEditDistanceArray.java
+++ src/main/java/TreeEditDistanceArray.java
@@ -12,7 +12,7 @@ public class TreeEditDistanceArray extends TreeEditDistance{
 
     public double[] compute() throws Exception {
         File[] listOfFiles = getFiles();
-        if (listOfFiles.length == 0){
+        if (getFiles().length == 0){
             return null;
         }
         LblTree lt1 = buildTree(listOfFiles[listOfFiles.length - 1]);
